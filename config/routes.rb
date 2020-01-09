ea164d652b69c8c435618e701f574991fa3aaa2e
nathanveach <nathanbveach@gmail.com>
2020-01-09 15:16:15 -0800

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :restaurants
+	root 'restaurants#index'
+  get 'pages/about'
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
