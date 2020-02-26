6fe105862916230e9e0e05d06a944510593798b8
nathanveach <nathanbveach@gmail.com>
2020-02-26 14:09:58 -0800

    Added carrierwave gem and code for image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_223047) do
+ActiveRecord::Schema.define(version: 2020_02_26_215235) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_223047) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
